756b2f7b96d43f8a0008c88d2deb6e96a35813b7
ZoneSix <areazone6@gmail.com>
2018-12-13 09:53:06 +0100

    WebSocket server revision

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-// JSON.stringify(data)
-// JSON.parse(data)
-
-// Import modules
+/*** Import modules ***/
 const isJSON      = require('is-valid-json'),
       WebSocket   = require('ws'),
       fileSystem  = require('fs');
 
-// Import Configuration file
+/*** Import Configuration file ***/
 let config = {
   path: './server.config.json'
 };
 
+// Load config file as JSON if it exists
 if (fileSystem.existsSync(config.path)) {
-  config = JSON.parse(fileSystem.readFileSync(config.path, 'utf8'));
+  let configImport = fileSystem.readFileSync(config.path, 'utf8');
+
+  // Check if config file is in a valid JSON format
+  if (isJSON(configImport)) {
+    config = JSON.parse(configImport);
+  }
+
+  // Output error to console if it isn't a valid JSON format
+  else {
+    console.error('Invalid JSON format in configuration file');
+  }
 }
 
-// Declare constant variables
+/*** Declare constant variables ***/
 const wsPort          = config.wsPort || 80,  // The port the WebSocket server listens on
       clients         = [],         // Array of Clients
       wsFunctions     = {},         // Object of server request functions
       wsFunctionsMap  = new Map();  // Map of server request functions
 
-// General functions
+/*** General functions ***/
+
+// Checks if input variable is declared and set
 const isSet = variable => {
   return !(typeof variable === 'undefined' || variable === null);
 };
@@ -37,7 +47,9 @@ const init = () => {
   startWSServer(WebSocket);
 };
 
-// Server functions
+/*** Server functions ***/
+
+// Start the WebSocket server
 const startWSServer = webSocket => {
   console.info(`Starting WebSocket server on port ${wsPort}`);
 
@@ -50,6 +62,7 @@ const startWSServer = webSocket => {
   wss.on('connection', wsConnection);
 };
 
+// Handle connection
 const wsConnection = ws => {
   console.log('[WS] Connection');
 
@@ -61,118 +74,195 @@ const wsConnection = ws => {
     };
     wsMessage(request);
   });
-  ws.on('close', wsClose);
+
+  // Handle disconnects
+  ws.on('close', () => {
+    wsClose(ws);
+  });
 };
 
+// Handle requests from clients
 const wsMessage = message => {
   console.log('[WS] Message Recieved:', message.content);
+
+  // Validate JSON format from client
   if (isJSON(message.content)) {
     incomingMessage(message.client, JSON.parse(message.content));
   }
 
+  // Send back an error if the request isn't a valid JSON format
   else {
     incomingMessageError(message.client, `Invalid JSON format`);
   }
 };
 
+// Handle client disconnect
 const wsClose = client => {
-  wsFunctionsMap.get('leaveChat')(client);
+  wsFunctions.leaveChat(client);
   console.log('[WS] Disconnected');
 };
 
+// Handle client request
 const incomingMessage = (client, data) => {
+
+  // Check if the action key is set
   if (!isSet(data.action)) {
     incomingMessageError(client, `action is missing`);
     return;
   }
 
+  // Check if requested action exists
   if (!wsFunctionsMap.has(data.action)) {
     incomingMessageError(client, `No such action`);
     return;
   }
 
+  // Run requested action
   wsFunctionsMap.get(data.action)(client, data);
 };
 
+// Send error back to client
 const incomingMessageError = (client, error) => {
   client.send(JSON.stringify({
     error: error
   }));
 };
 
+// Broadcast to all clients
 const broadcast = data => {
+
+  // Check if there are any clients joined in the chat before broadcasting
   if (clients.length > 0) {
-    clients.forEach(client => {
-      client.send(JSON.stringify(data));
+    clients.forEach((client, index) => {
+
+      // Make a copy of the broadcast data for targeted client adaptions
+      let dataInstance = JSON.parse(JSON.stringify(data));
+
+      // Remove the client object from targeted broadcast data
+      // We don't want to send the client object to the clients
+      delete dataInstance.client;
+
+      // Set a 'me' flag if the broadcast is triggered by this client
+      if (data.client && Object.is(client, data.client)) {
+        dataInstance.data.me = true;
+      }
+
+      // Make sure the client is still connected before sending the braodcast to it
+      if (client.readyState === WebSocket.OPEN) {
+        // Send the broadcast data to the client
+        client.send(JSON.stringify(dataInstance));
+      }
+
+      // Remove the client from the client list if the connection is broken
+      else {
+        clients.splice(index, 1);
+        console.warn('Found & deleted closed client on index', index);
+      }
     });
   }
 };
 
-// Server request functions
-wsFunctions.joinChat = (client, data) => {
-  if (!isSet(data.nickname)) {
-    incomingMessageError(client, `nickname is missing`);
-    return;
-  }
+/*** Server request functions ***/
 
+// Join client to the chat
+wsFunctions.joinChat = (client, data) => {
+
+  // Check if the client has already joined the chat
   if (clients.indexOf(client) != -1) {
     incomingMessageError(client, `Client already joined`);
     return;
   }
 
-  client.nickname = data.nickname;
-  broadcast({
-    action: 'broadcast',
-    message: `${data.nickname} has joined!`
-  });
-  clients.push(client);
-  client.send(JSON.stringify({
-    action: 'broadcast',
-    message: `Welcome ${data.nickname}!`
-  }));
-};
-
-wsFunctions.leaveChat = (client) => {
-  if (clients.length) {
-    let index = clients.indexOf(client);
-    if (index != -1) {
-      let nickname = client.nickname;
-      clients.splice(index, 1);
-      broadcast({
-        action: 'broadcast',
-        message: `${nickname} has left.`
-      });
-    }
-  }
-};
-
-wsFunctions.sendChat = (client, data) => {
-  if (!isSet(data.message)) {
-    incomingMessageError(client, `message is missing`);
-    return;
-  }
-
-  broadcast({
-    action: 'incomingChat',
-    data: {
-      time: Date.now(),
-      name: client.nickname,
-      message: data.message
-    }
-  });
-};
-
-wsFunctions.changeNickname = (client, data) => {
+  // Check if the nickname is set
   if (!isSet(data.nickname)) {
     incomingMessageError(client, `nickname is missing`);
     return;
   }
 
-  let oldNickname = client.nickname;
+  // Include the nickname in the client object
   client.nickname = data.nickname;
+  // Add the client to the joined clients array
+  clients.push(client);
+
+  // Send a welcome to the joined client
+  client.send(JSON.stringify({
+    action: 'welcomeClient',
+    nickname: data.nickname,
+    message: `Welcome ${data.nickname}!`,
+  }));
+
+  // Broadcast the joined client
   broadcast({
-    action: 'broadcast',
-    message: `${oldNickname} is now known as ${data.nickname}`
+    action: 'clientJoin',
+    nickname: data.nickname,
+  });
+};
+
+// Remove client from the chat
+wsFunctions.leaveChat = (client) => {
+
+  // Make sure the client array isn't empty
+  if (clients.length > 0) {
+
+    // Get the index of client object
+    let index = clients.indexOf(client);
+
+    // Remove client from chat if found
+    if (index != -1) {
+      // Save the nickname before removing
+      let nickname = client.nickname;
+      // Remove client from the chat
+      clients.splice(index, 1);
+
+      // Broadcast client leaving
+      broadcast({
+        action: 'clientLeave',
+        nickname: nickname,
+      });
+    }
+  }
+};
+
+// Handle incoming chat message
+wsFunctions.sendChat = (client, data) => {
+
+  // Check if a message exists
+  if (!isSet(data.message)) {
+    incomingMessageError(client, `message is missing`);
+    return;
+  }
+
+  // Broadcast the chat message
+  broadcast({
+    client: client,
+    action: 'incomingChat',
+    data: {
+      time: Date.now(),
+      name: client.nickname,
+      content: data.message,
+    }
+  });
+};
+
+// Handle client nickname change
+wsFunctions.changeNickname = (client, data) => {
+
+  // Check if nickname is set
+  if (!isSet(data.nickname)) {
+    incomingMessageError(client, `nickname is missing`);
+    return;
+  }
+
+  // Save old nickname
+  let oldNickname = client.nickname;
+  // Set new nickname to client
+  client.nickname = data.nickname;
+
+  // Broadcast the nickname change
+  broadcast({
+    action: 'nicknameChange',
+    oldNickname: oldNickname,
+    newNickname: data.nickname,
   });
 };
 
